527e6b9a9629e71e99a6103829623733d4f92bff
aziemelis <aziemelis@proton.me>
2026-01-12 21:33:26 +0200

    Update .gitignore to include IDE and build tool configurations

---
+++ src/main/java/com/aziemelis/littleleauge/LittleleaugeApplication.java
@@ -0,0 +1,13 @@
+package com.aziemelis.littleleauge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleleaugeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleleaugeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aziemelis/littleleauge/LittleleaugeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziemelis.littleleauge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleleaugeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
